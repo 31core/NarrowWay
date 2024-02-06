4d38b5d6bd0543ff7d973c1a001e3cbdccc87824
31core <31core@tutanota.com>
2024-02-06 21:44:37 +0800

    Modify src/exports.rs

--- include/NarrowWay.h
+++ include/NarrowWay.h
@@ -6,7 +6,10 @@ typedef void * Cipher256;
 
 Cipher128 new_cipher_128(uint8_t*);
 void encrypt_128(Cipher128, uint8_t*, uint8_t*);
-Cipher192 new_cipher_128(uint8_t*);
-void encrypt_128(Cipher192, uint8_t*, uint8_t*);
-Cipher256 new_cipher_128(uint8_t*);
-void encrypt_128(Cipher256, uint8_t*, uint8_t*);
+void decrypt_128(Cipher128, uint8_t*, uint8_t*);
+Cipher192 new_cipher_192(uint8_t*);
+void encrypt_192(Cipher192, uint8_t*, uint8_t*);
+void decrypt_192(Cipher128, uint8_t*, uint8_t*);
+Cipher256 new_cipher_256(uint8_t*);
+void encrypt_256(Cipher256, uint8_t*, uint8_t*);
+void decrypt_256(Cipher128, uint8_t*, uint8_t*);
--- src/exports.rs
+++ src/exports.rs
@@ -32,6 +32,19 @@ pub extern "C" fn encrypt_128(cipher: *const u8, plaintext_raw: *const u8, buf: 
 }
 
 #[no_mangle]
+pub extern "C" fn decrypt_128(cipher: *const u8, plaintext_raw: *const u8, buf: *mut u8) {
+    let cipher = unsafe { &*(cipher as *const Cipher128) };
+    let mut plaintext = [0; 16];
+    for (i, byte) in plaintext.iter_mut().enumerate() {
+        *byte = unsafe { *plaintext_raw.add(i) };
+    }
+    let cipher_text = cipher.decrypt(plaintext);
+    for (i, byte) in cipher_text.iter().enumerate() {
+        unsafe { *buf.add(i) = *byte };
+    }
+}
+
+#[no_mangle]
 pub extern "C" fn new_cipher_192(key_raw: *const u8) -> *const u8 {
     let mut key = [0; 24];
     for (i, byte) in key.iter_mut().enumerate() {
@@ -62,6 +75,19 @@ pub extern "C" fn encrypt_192(cipher: *const u8, plaintext_raw: *const u8, buf: 
 }
 
 #[no_mangle]
+pub extern "C" fn decrypt_192(cipher: *const u8, plaintext_raw: *const u8, buf: *mut u8) {
+    let cipher = unsafe { &*(cipher as *const Cipher192) };
+    let mut plaintext = [0; 24];
+    for (i, byte) in plaintext.iter_mut().enumerate() {
+        *byte = unsafe { *plaintext_raw.add(i) };
+    }
+    let cipher_text = cipher.decrypt(plaintext);
+    for (i, byte) in cipher_text.iter().enumerate() {
+        unsafe { *buf.add(i) = *byte };
+    }
+}
+
+#[no_mangle]
 pub extern "C" fn new_cipher_256(key_raw: *const u8) -> *const u8 {
     let mut key = [0; 32];
     for (i, byte) in key.iter_mut().enumerate() {
@@ -86,6 +112,19 @@ pub extern "C" fn encrypt_256(cipher: *const u8, plaintext_raw: *const u8, buf: 
         *byte = unsafe { *plaintext_raw.add(i) };
     }
     let cipher_text = cipher.encrypt(plaintext);
+    for (i, byte) in cipher_text.iter().enumerate() {
+        unsafe { *buf.add(i) = *byte };
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn decrypt_256(cipher: *const u8, plaintext_raw: *const u8, buf: *mut u8) {
+    let cipher = unsafe { &*(cipher as *const Cipher256) };
+    let mut plaintext = [0; 32];
+    for (i, byte) in plaintext.iter_mut().enumerate() {
+        *byte = unsafe { *plaintext_raw.add(i) };
+    }
+    let cipher_text = cipher.decrypt(plaintext);
     for (i, byte) in cipher_text.iter().enumerate() {
         unsafe { *buf.add(i) = *byte };
     }
